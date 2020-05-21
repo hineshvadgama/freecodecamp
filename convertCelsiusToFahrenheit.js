cfad129a397eda51458d4d0c428a18316aa93a80
hineshvadgama <hinesh.vadgama@outlook.com>
2020-05-21 16:40:11 +0100

    challenge 1 and 2

---
+++ convertCelsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+function convertToF(celsius) {
+    let fahrenheit = celsius;
+    fahrenheit = fahrenheit * 9 / 5 + 32;
+    return fahrenheit;
+  }
+  
+  convertToF(30);
+  
---
+++ reverseAString.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    let stringAsArray = str.split("");
+    stringAsArray = stringAsArray.reverse();
+    str = stringAsArray.join("");
+    return str;
+  }
+  
+  reverseString("hello");
+  
